94e9ad0709c32230be6b6dc20e3269077fac72b9
Brogy-alt <brogangys2@gmail.com>
2023-03-03 11:28:00 +0200

    updated

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="card" style="width: 18rem;" v-for="product in products" :key="product.id">
+            <img :src="product.imgURL" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ product.prodName }}</h5>
+                <p class="card-text">{{ product.prodGenre }}</p>
+                <p class="card-text">{{ product.price }}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        products() {
+            return this.$store.state.products;
+        }
+    },
+    mounted(){
+        this.$store.dispatch('fetchProducts')
+    }
+}
+</script>
